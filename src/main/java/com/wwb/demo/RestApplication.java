2cf0546beaeac3a9f8dfd0e589dac3c6414b7367
wwb <382899012@qq.com>
2018-11-29 17:00:11 +0800

    静态资源、多环境切换

--- src/main/java/com/wwb/demo/RestApplication.java
+++ src/main/java/com/wwb/demo/RestApplication.java
@@ -1,15 +1,31 @@
 package com.wwb.demo;
 
+
+import org.junit.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.classic.Logger;
+
+//主配置类
 @SpringBootApplication
 public class RestApplication {
 
+	Logger logger = (Logger) LoggerFactory.getLogger(RestApplication.class);
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
 		
 		System.out.println("============== demo start  success =============");
+
+	}
+	
+	@Test
+	public void contextLoads() {
+		
+		logger.error("错误日志...");
+		
 	}
 }
--- src/test/java/com/wwb/demo/DemoApplicationTests.java
+++ src/test/java/com/wwb/demo/DemoApplicationTests.java
@@ -2,15 +2,23 @@ package com.wwb.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import ch.qos.logback.classic.Logger;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	Logger logger = (Logger) LoggerFactory.getLogger(RestApplication.class);
+	
 	@Test
 	public void contextLoads() {
+		
+		logger.error("错误。。。");
+		
 	}
 
 }
