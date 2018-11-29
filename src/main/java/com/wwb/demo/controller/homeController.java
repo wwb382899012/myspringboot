5bab6f5cf12c7827e05d317210b76f56e3aefbde
wwb <382899012@qq.com>
2018-11-29 18:02:23 +0800

    thymeleaf模板

--- src/main/java/com/wwb/demo/controller/homeController.java
+++ src/main/java/com/wwb/demo/controller/homeController.java
@@ -1,15 +1,42 @@
 package com.wwb.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.wwb.demo.entity.Person;
+
 @Controller
 public class homeController {
+	
+	/**
+	 * 默认欢迎也
+	 * @return
+	 */
 	@RequestMapping("/")
 	@ResponseBody
 	public String index(){
 		return "欢迎，这是首页";
+		
+	}
+	
+	
+	@RequestMapping("/temp")
+	public String welcome(Map<String,Object> map){
+		map.put("name", "我被替换了");
+		map.put("id", "p_id");
+		
+		
+		List<Person> persons = new ArrayList<>();
+		persons.add(new Person(1,"温文斌",21));
+		persons.add(new Person(1,"yyy",22));
+		persons.add(new Person(1,"xxj",23));
+		map.put("persons",persons);
+		return "temp";//加载html文件
 		
 	}
 }
