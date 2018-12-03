77d2bce693821d906d5a39197a812597b8a7aecf
wwb <382899012@qq.com>
2018-12-03 17:41:28 +0800

    mybatis-generator自动生成mapper、xml文件

---
+++ src/main/java/com/wwb/demo/mapper/AppuserMessage.java
@@ -0,0 +1,56 @@
+package com.wwb.demo.mapper;
+
+import javax.persistence.*;
+
+@Table(name = "appuser_message")
+public class AppuserMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String message;
+
+    private Integer sendtime;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * @return sendtime
+     */
+    public Integer getSendtime() {
+        return sendtime;
+    }
+
+    /**
+     * @param sendtime
+     */
+    public void setSendtime(Integer sendtime) {
+        this.sendtime = sendtime;
+    }
+}
---
+++ src/main/java/com/wwb/demo/mapper/mapper/PersonMapper.java
@@ -0,0 +1,7 @@
+package com.wwb.demo.mapper.mapper;
+
+import com.wwb.demo.mapper.Person;
+import com.wwb.demo.mapper.util.MyMapper;
+
+public interface PersonMapper extends MyMapper<Person> {
+}
