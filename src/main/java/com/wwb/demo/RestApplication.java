fdbf8b74e1d35b90f73e90bb37b7736534782f9a
wwb <382899012@qq.com>
2018-12-04 15:36:20 +0800

    定时任务: task

--- src/main/java/com/wwb/demo/RestApplication.java
+++ src/main/java/com/wwb/demo/RestApplication.java
@@ -6,12 +6,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import ch.qos.logback.classic.Logger;
 
 //主配置类
 @SpringBootApplication
 @MapperScan("com.wwb.demo.mapper")
+@EnableScheduling
 public class RestApplication {
 
 	Logger logger = (Logger) LoggerFactory.getLogger(RestApplication.class);
---
+++ src/main/java/com/wwb/demo/task/TaskController.java
@@ -0,0 +1,20 @@
+package com.wwb.demo.task;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+//打开注释即开启
+//@Component
+public class TaskController {
+
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+	
+	@Scheduled(fixedRate=3000)
+	public void reportCurrentDate(){
+		System.out.println("现在时间："+dateFormat.format(new Date()));
+	}
+	
+}
