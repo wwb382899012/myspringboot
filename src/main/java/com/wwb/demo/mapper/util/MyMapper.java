05665a918d2d6fe02ed42246c4114f602fe98c83
wwb <382899012@qq.com>
2018-12-05 17:44:17 +0800

    Mybatis Generator 生成xml文件

--- src/main/java/com/wwb/demo/mapper/util/MyMapper.java
+++ src/main/java/com/wwb/demo/mapper/util/MyMapper.java
@@ -6,4 +6,7 @@ import tk.mybatis.mapper.common.MySqlMapper;
 public interface MyMapper<T> extends Mapper<T>,MySqlMapper<T> {
     //TODO
     //FIXME 特别注意，该接口不能被扫描到，否则会出错
+	
+	int deleteByPrimaryKey(Integer id);
+	
 }
